72de6dcb56bcde5a7c8d00f35a540f02a1f2e967
kaeyeh <mantr8ce>
2017-04-09 21:42:29 -0700

    update readme

--- src/app/comp2/comp2.component.ts
+++ src/app/comp2/comp2.component.ts
@@ -4,6 +4,7 @@
   import { templates } from '../service/template.service';
   import { UserService, User } from '../service/user.service';
   import {Observable} from "rxjs/Observable";
+  import 'rxjs/add/operator/reduce';
 
   @Component({
     selector: 'app-comp2',
@@ -12,36 +13,37 @@
     providers: [UserService]
   })
   export class Comp2Component implements OnInit {
-
+    // Remove the states to prevent defects, but I have to leave on for catching
+    // HTTP error, so far I have no way to handle error with async pipe
     private users$ : Observable<any>;
-    private tail$ : Observable<any>;
+    //private tail$ : Observable<any>;
     private hero: User;
     private id = 0;
     constructor( private user: UserService) {
     }
 
     ngOnInit() {
+      // Remove the states to prevent defects
+      this.users$ = this.user.load$();
+      // this.tail$ = this.user.tail$(95);
 
+      // handle error
+      this.users$.subscribe(
+        data => {},
+        this._error
+      );
 
-    this.users$ = this.user.load$();
-    this.tail$ = this.user.tail$(95);
-
-    // handle error
-    this.users$.subscribe(
-      data => {},
-      this._error
-    );
-
-
-
-
-
-      //this.user$.catch(this._error);
     }
 
     _error( err: any) {
       console.debug(err);
     }
+
+
+    private tail$ () {
+      return this.user.tail$(95);
+    }
+
 
     getHero() {
       this.id++;
